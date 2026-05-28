466a1050fdd3e6213c4bd7444bf322f106e811c7
octpsmon <78450868+octpsmon@users.noreply.github.com>
2026-05-28 17:27:10 +0200

    Update dicomUSGvol3.m

--- dicom_segmentacja/dicomUSGvol3.m
+++ dicom_segmentacja/dicomUSGvol3.m
@@ -1,17 +1,15 @@
 function dicomUSGvol3
 % dicomUSGvol3
 %
-% This file is not included in the public repository.
 % The original implementation was developed as a merged and
 % extended version of a pre-existing MATLAB tool.
 %
 % The master's thesis contribution concerned major additions in the form of
 % automatic segmentation workflow and its integration with the GUI.
-% However, because this file also incorporated earlier code, the full
-% implementation is not redistributed here.
+% However, because this file also incorporated earlier code belonging to another researcher, the full
+% implementation is not presented here.
 %
-% Please refer to the repository documentation for a description of the
-% added functionality.
+% Please refer to the repository documentation regarding the added functionalities.
 
-error('dicomUSGvol3.m is not distributed in the public repository.');
+error('dicomUSGvol3.m is not distributed in the current repository.');
 end
